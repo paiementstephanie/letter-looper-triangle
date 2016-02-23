b4fbbce84da0d356417b8648cbc748c799460e1c
paiementstephanie <stephanie.paiement@rogers.com>
2016-02-23 17:34:49 -0500

    indentation and spacing

--- js/main.js
+++ js/main.js
@@ -4,10 +4,10 @@ var i;
 
 var j;
 
-for (i = 1; i < 11; i++) {
+for (i = 0; i < 10; i++) {
 
-for (j = 0; j < i; j++) {
-  document.write(newLetter);
-}
+  for (j = 0; j < i; j++) {
+    document.write(newLetter);
+  }
   document.write('<br>');
 }
